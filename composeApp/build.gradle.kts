9bff2aa7e3457892955fae89d0c44328c1912861
Jérôme Dusanter <dusanter.jerome@gmail.com>
2024-10-14 09:59:15 +0200

    Increment version name to 1.1

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -63,8 +63,8 @@ android {
         applicationId = "com.jeromedusanter.youonlyneedcards"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
     packaging {
         resources {
