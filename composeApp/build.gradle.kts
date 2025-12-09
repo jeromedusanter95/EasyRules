f9c757c229ce6840eebe99782121a0e085a3e15f
Jérôme Dusanter <dusanter.jerome@gmail.com>
2025-12-09 16:24:39 +0100

    Increase version name to 1.2

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -68,8 +68,8 @@ android {
         applicationId = "com.jeromedusanter.easyrules"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
     }
     packaging {
         resources {
